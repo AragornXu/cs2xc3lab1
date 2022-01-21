51a3a6650cce24be6a75b36ffa8d5ef907433d6a
Lynn-YYYUUU <yu19@mcmaster.ca>
2022-01-21 13:25:23 +0800

    Changing are_valid_groups function

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
 def are_valid_groups(studentNumbers, groups):
     for eachGroup in groups:
+        if (len(eachGroup) != 2 and len(eachGroup) != 3):
+            return False
         for eachStudent in eachGroup:
             try:
                 studentNumbers.remove(eachStudent)
