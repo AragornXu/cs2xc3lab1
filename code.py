b3a0c16b18a6866f866b9fc93085545c561ddc6d
Lynn-YYYUUU <yu19@mcmaster.ca>
2022-01-21 13:36:19 +0800

    Resolve conflicts

--- code.py
+++ code.py
@@ -1,11 +1,10 @@
 def are_valid_groups(studentNumbers, groups):
-    return False
     for eachGroup in groups:
         if (len(eachGroup) != 2 and len(eachGroup) != 3):
             return False
         for eachStudent in eachGroup:
-            expect:
-                studentNumbers.remove(eachStudent)
             try:
+                studentNumbers.remove(eachStudent)
+            except:
                 return False
     return len(studentNumbers) == 0
