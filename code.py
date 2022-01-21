1f00f75a1a1316dd4dfc1ae3a5fa87d5608ee816
AragornXu <2101909718@qq.com>
2022-01-21 00:43:20 -0500

    Revert "Changing are_valid_groups function"
    This reverts commit 51a3a6650cce24be6a75b36ffa8d5ef907433d6a.

--- code.py
+++ code.py
@@ -1,8 +1,6 @@
 def are_valid_groups(studentNumbers, groups):
     return False
     for eachGroup in groups:
-        if (len(eachGroup) != 2 and len(eachGroup) != 3):
-            return False
         for eachStudent in eachGroup:
             expect:
                 studentNumbers.remove(eachStudent)
