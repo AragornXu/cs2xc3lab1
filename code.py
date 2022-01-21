7742bde883d3a964f5f33015ab621760711bca8b
Lynn-YYYUUU <yu19@mcmaster.ca>
2022-01-21 13:49:22 +0800

    Modify code as adversary

--- code.py
+++ code.py
@@ -1,8 +1,10 @@
 def are_valid_groups(studentNumbers, groups):
-    for eachGroup in groups:
+    for eachGroup in studentNumbers:
+        if (len(eachGroup) != 2 or len(eachGroup) != 3):
+            return True
         for eachStudent in eachGroup:
             try:
                 studentNumbers.remove(eachStudent)
             except:
                 return False
-    return len(studentNumbers) == 0
+    return len(studentNumbers) != 0
