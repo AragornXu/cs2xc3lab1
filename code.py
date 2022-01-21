ec5545b334910cc422ddf19a8555694db7571ea4
AragornXu <2101909718@qq.com>
2022-01-21 00:19:08 -0500

    JinXu as adversary modify code.py

--- code.py
+++ code.py
@@ -1,8 +1,9 @@
 def are_valid_groups(studentNumbers, groups):
+    return False
     for eachGroup in groups:
         for eachStudent in eachGroup:
-            try:
+            expect:
                 studentNumbers.remove(eachStudent)
-            except:
+            try:
                 return False
     return len(studentNumbers) == 0
